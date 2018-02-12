1a33fca90174c0fbb1f6699ec564b1db10ef5106
javecruz <clony90@gmail.com>
2018-02-12 13:34:48 +0100

    sql added wwith data

--- ClientesApp/empresa.sql
+++ ClientesApp/empresa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-02-2018 a las 12:02:26
+-- Tiempo de generación: 12-02-2018 a las 13:33:16
 -- Versión del servidor: 10.1.21-MariaDB
 -- Versión de PHP: 5.6.30
 
